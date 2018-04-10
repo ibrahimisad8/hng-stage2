ff77efb50018ef81373dbb449ca6e80c73b14bfa
Ibrahim <hibsoft@yahoo.com>
2018-04-10 19:40:40 +0100

    Landing page

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+/**
+ * Simple page | Hng Internship 2018
+ * 
+ * @author : Ibrahim Isa
+ * @version : 0.1
+ */
+?>
+<html>
+<head>
+    <title>Hng Internship 2018</title>
+    <link href="style.css" rel="stylesheet" />
+    <link rel="icon" href="favicon.png">
+</head>
+<body>
+    <div class="wrapper">
+        <div id="left">
+            <img class="image" src="tea.jpg" />
+        </div>
+        <div id="right"> 
+            <p class="time" style="padding-left:0px">TIME</p>
+            <p class="time-310"><?php echo date('h:m:s a'); ?></p>
+            <p class="hotelsng">hotelsng</p>
+            <p class="hng-internship-4">Hng Internship 4</p>
+        </div>
+    </div>
+</body>
+</html>
